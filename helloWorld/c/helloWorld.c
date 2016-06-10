35766e3727e263103e4de9c1e4d1cb6c4c6e9cfc
JairoGarcia <woodensnake@gmail.com>
2016-06-10 18:07:03 +0200

    Added hello world written in C

---
+++ helloWorld/c/helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+  Example of Hello World written in C
+
+  To compile:
+    $> gcc -o output helloWorld.c
+*/
+
+int main (int argc, char **argv){
+  printf("Hello, world\n");
+}
