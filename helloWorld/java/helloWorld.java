f26f20985d11a5ddc094d0b23abfb9336d768cd0
JairoGarcia <woodensnake@gmail.com>
2016-06-10 18:50:06 +0200

    Added hello world written in java

---
+++ helloWorld/java/helloWorld.java
@@ -0,0 +1,17 @@
+package java;
+
+import java.io.IOException;
+
+/*
+  Example of Hello World written in Java
+*/
+
+public class helloWorld{
+  public static void main(String[] args) throws IOException{
+    sayHello();
+  }
+
+  public static void sayHello(){
+    System.out.println("Hello, world");
+  }
+}
